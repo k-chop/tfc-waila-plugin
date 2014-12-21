ff5d5b9b8968012c12a6a4ba44f5bd49f7c6fed2
whelmaze <whelmaze@gmail.com>
2014-12-22 05:05:34 +0900

    Fix NPE in CropProvider (TFC B79.14) and add nutrient tooltip
    Of course, need expert agriculture skill :)

--- src/main/scala/tfcwailaplugin/CropProvider.scala
+++ src/main/scala/tfcwailaplugin/CropProvider.scala
@@ -2,13 +2,16 @@ package tfcwailaplugin
 
 import java.util.{List => JList}
 
-import com.bioxx.tfc.Food.{CropIndexPepper, CropIndex, CropManager}
+import com.bioxx.tfc.Core.Player.SkillStats.SkillRank
+import com.bioxx.tfc.Core.TFC_Core
+import com.bioxx.tfc.Food.{ItemFoodTFC, CropIndexPepper, CropIndex, CropManager}
 import com.bioxx.tfc.TileEntities.TECrop
+import com.bioxx.tfc.api.Constant.Global
 import mcp.mobius.waila.api.{IWailaConfigHandler, IWailaDataAccessor}
 import net.minecraft.item.ItemStack
 
 
-object CropProvider extends ProviderBase[TECrop] with Cacheable[Int, ItemStack] {
+object CropProvider extends ProviderBase[TECrop] with Cacheable[Int, String] {
 
   override def getWailaHead(stack: ItemStack,
                    tooltip: JList[String],
@@ -17,14 +20,25 @@ object CropProvider extends ProviderBase[TECrop] with Cacheable[Int, ItemStack] 
     import net.minecraft.util.EnumChatFormatting._
 
     accessor.getTileEntity match {
-      case e: TECrop =>
-        val is = CropManager.getInstance.getCropFromId(e.cropId) match {
-          case pepper: CropIndexPepper => cache.getOrElseUpdate(e.cropId, new ItemStack(pepper.Output2))
-          case others: CropIndex => cache.getOrElseUpdate(e.cropId, new ItemStack(others.Output1))
-          case _ => null
+      case tec: TECrop =>
+        val str = CropManager.getInstance.getCropFromId(tec.cropId) match {
+          case pepper: CropIndexPepper if pepper.Output2 != null =>
+            cache.getOrElseUpdate(tec.cropId,{
+              val newIs = new ItemStack(pepper.Output2, 1)
+              ItemFoodTFC.createTag(newIs, 1.0f)
+              newIs.getDisplayName
+            })
+          case others: CropIndex if others.Output1 != null =>
+            cache.getOrElseUpdate(tec.cropId, {
+              val newIs = new ItemStack(others.Output1, 1)
+              ItemFoodTFC.createTag(newIs, 1.0f)
+              newIs.getDisplayName
+            })
+          case _ =>
+            "Unknown"
         }
-        if (is != null && !tooltip.isEmpty) {
-          tooltip.set(0, s"${WHITE}Crop (${is.getDisplayName})")
+        if (!tooltip.isEmpty) {
+          tooltip.set(0, s"${WHITE}Crop ($str)")
         }
       case _ =>
     }
@@ -34,6 +48,26 @@ object CropProvider extends ProviderBase[TECrop] with Cacheable[Int, ItemStack] 
   override def getWailaBody(stack: ItemStack,
                    tooltip: JList[String],
                    accessor: IWailaDataAccessor,
-                   config: IWailaConfigHandler): JList[String] = tooltip
+                   config: IWailaConfigHandler): JList[String] = {
+    import net.minecraft.util.EnumChatFormatting._
+    import net.minecraft.util.StatCollector.translateToLocal
+
+    // https://github.com/Deadrik/TFCraft/commit/055d3559bbfc257b0b4f6e0e8384631be661bf11
+    val rank = TFC_Core.getSkillStats(accessor.getPlayer).getSkillRank(Global.SKILL_AGRICULTURE)
+    if (rank == SkillRank.Expert || rank == SkillRank.Master) {
+      val nuType = accessor.getTileEntity match {
+        case tec: TECrop =>
+          CropManager.getInstance.getCropFromId(tec.cropId).getCycleType match {
+            case 0 => s"$RED${translateToLocal("gui.Nutrient.A")}"
+            case 1 => s"$GOLD${translateToLocal("gui.Nutrient.B")}"
+            case 2 => s"$YELLOW${translateToLocal("gui.Nutrient.C")}"
+            case _ => ""
+          }
+        case _ => ""
+      }
+      tooltip.add(nuType)
+    }
+    tooltip
+  }
 
 }
